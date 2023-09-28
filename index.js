a9b1b43792450c3f1bf1ea911d5b69908ce114c6
Gambu101 <gabriel.ohalete.244627@unn.edu.ng>
2023-09-28 18:55:49 +0100

    mobile view only (first commi)
    The Css here is for mobile view only, desktop view is currently in building

---
+++ index.js
@@ -0,0 +1,23 @@
+let body = document.querySelector("body")
+let input = document.querySelectorAll("input")
+
+let rating_container = document.getElementById("rating_container")
+
+let thank_you_page = document.getElementById("thank_you_container")
+
+let rate = document.getElementById("rate")
+
+body = thank_you_page.style.display = "none"
+
+
+function rating(number){
+    let btn_value = number.innerText;
+    rate.innerHTML = btn_value
+    
+}
+
+function submit(){
+    thank_you_page.style.display = "block"
+    rating_container.style.display = "none"
+    rating()
+}
